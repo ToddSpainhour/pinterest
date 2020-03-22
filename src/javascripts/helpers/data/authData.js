3941b247abbae79652a51ed1585a84bedd317ca5
Todd Spainhour <toddspainhour@gmail.com>
2020-03-22 14:50:46 -0500

    logout button added, greeting and boards divs added

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -4,16 +4,19 @@ import 'firebase/auth';
 const authDiv = $('#auth');
 const userPinCollectionDiv = $('#print-user-pin-collection-here');
 const logoutButton = $('#navbar-logout-button');
+const pinterestGreetingDiv = $('#pinterest-greeting-div');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in
+      pinterestGreetingDiv.addClass('hide');
       authDiv.addClass('hide');
       userPinCollectionDiv.removeClass('hide');
       logoutButton.removeClass('hide');
     } else {
       // person is NOT logged in
+      pinterestGreetingDiv.removeClass('hide');
       authDiv.removeClass('hide');
       userPinCollectionDiv.addClass('hide');
       logoutButton.addClass('hide');
