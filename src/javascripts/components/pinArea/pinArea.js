7ec88a08b671d3037ebf1c5c89eecd22be3bd58e
Todd Spainhour <toddspainhour@gmail.com>
2020-03-28 12:56:18 -0500

    getPins returning only needed data console

--- src/javascripts/components/pinArea/pinArea.js
+++ src/javascripts/components/pinArea/pinArea.js
@@ -2,7 +2,7 @@ import pinData from '../../helpers/data/pinData';
 
 const buildPins = () => {
   pinData.getPins()
-    .then((response) => console.error('getPins worked', response))
+    .then((response) => console.error('getPins worked', response.data))
     .catch((err) => console.error('getPins broke', err));
 };
 
