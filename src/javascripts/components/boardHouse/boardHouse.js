1664a4e115aa49a217c2309b83aae307a1b8cdba
Todd Spainhour <toddspainhour@gmail.com>
2020-04-04 14:49:02 -0500

    board cards displaying

--- src/javascripts/components/boardHouse/boardHouse.js
+++ src/javascripts/components/boardHouse/boardHouse.js
@@ -1,5 +1,20 @@
+import boardData from '../../helpers/data/boardData';
+import boardComponent from '../board/board';
+import utils from '../../helpers/utils';
+
 const buildBoards = () => {
-  console.error('you made some boards');
+  boardData.getBoards()
+    .then((boards) => {
+      let domString = '';
+      domString += '<h2 class="text-center">This is the Board Area</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      boards.forEach((board) => {
+        domString += boardComponent.boardMaker(board);
+      });
+      domString += '</div>';
+      utils.printToDom('print-boards-here', domString);
+    })
+    .catch((err) => console.error('whoops. there is a problem with your getBoards', err));
 };
 
 export default { buildBoards };
