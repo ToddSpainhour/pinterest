0a77164da2cff079244b831ac697bfef454cb067
Todd Spainhour <toddspainhour@gmail.com>
2020-04-07 17:53:45 -0500

    trying to only get certain board pins to show WIP

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -1,17 +1,38 @@
+import pinData from '../../helpers/data/pinData';
+// import utils from '../../helpers/utils';
+
 const userPinCollectionDiv = $('#print-user-pin-collection-here');
 const boardDiv = $('#print-boards-here');
 
+// const showOnlyThisBoardsPins = (e) => {
+//   const boardIdOnCard = e.target.closest('.card').id;
+//   if (boardIdOnCard === pinData.getPins().boardId) {
+//     console.error('it worked');
+//   } else {
+//     console.error('it did not work');
+//   }
+// };
 
-const showBoardPins = () => {
-  console.error('this is from within your showBoardPins function in the single boardJS file');
-  userPinCollectionDiv.removeClass('hide');
-  boardDiv.addClass('hide');
+
+const showBoardPins = (e) => {
+  const boardIdOnCard = e.target.closest('.card').id;
+  console.error(`the id of the board you clicked on is "${boardIdOnCard}"`);
+  if (boardIdOnCard === pinData.getPins().boardId) {
+    console.error('this is inside your if statement');
+    userPinCollectionDiv.removeClass('hide');
+    boardDiv.addClass('hide');
+  } else {
+    userPinCollectionDiv.removeClass('hide');
+    boardDiv.addClass('hide');
+    console.error('this is your else statement inside your showBoardPin function');
+    console.error('this the the promise?', pinData.getPins());
+  }
 };
 
+
 const backToBoards = () => {
-  console.error('this is from within your backToBoards function in the single boardJS file');
   userPinCollectionDiv.addClass('hide');
   boardDiv.removeClass('hide');
 };
 
-export default { showBoardPins, backToBoards };
+export default { backToBoards, showBoardPins };
