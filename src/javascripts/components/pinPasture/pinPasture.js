3e163f3eb6225668f77823cae1a91fc3abbd77ba
Todd Spainhour <toddspainhour@gmail.com>
2020-04-12 14:50:41 -0500

    pins displying in dom

--- src/javascripts/components/pinPasture/pinPasture.js
+++ src/javascripts/components/pinPasture/pinPasture.js
@@ -1,8 +1,19 @@
 import pinData from '../../helpers/data/pinData';
+import utils from '../../helpers/utils';
+import pinComponent from '../pins/pin';
 
 const buildPins = () => {
   pinData.getPins()
-    .then((response) => console.error('get pins worked!', response.data))
+    .then((pins) => {
+      let domString = '';
+      domString += '<h2 class="text-center">Pin Pasture</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      pins.forEach((pin) => {
+        domString += pinComponent.pinMaker(pin);
+      });
+      domString += '</div>';
+      utils.printToDom('print-pins-here', domString);
+    })
     .catch((err) => console.error('get pins broke', err));
 };
 
