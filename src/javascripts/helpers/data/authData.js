b69b01cbbf3f27d52e370bcfaaf279b0a0a4636d
Todd Spainhour <toddspainhour@gmail.com>
2020-04-12 15:01:43 -0500

    added board div

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -4,21 +4,24 @@ import 'firebase/auth';
 import pinPasture from '../../components/pinPasture/pinPasture';
 
 const authDiv = $('#auth');
-const pinPastureDiv = $('#print-pins-here');
 const logoutButton = $('#navbar-logout-button');
+const pinPastureDiv = $('#print-pins-here');
+const boardsDiv = $('#print-boards-here');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in
       authDiv.addClass('hide');
-      pinPastureDiv.removeClass('hide');
       logoutButton.removeClass('hide');
+      pinPastureDiv.removeClass('hide');
+      boardsDiv.removeClass('hide');
       pinPasture.buildPins();
     } else {
       authDiv.removeClass('hide');
-      pinPastureDiv.addClass('hide');
       logoutButton.addClass('hide');
+      pinPastureDiv.addClass('hide');
+      boardsDiv.addClass('hide');
     }
   });
 };
